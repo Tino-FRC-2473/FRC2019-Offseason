8ce8ac27ae66f2f1d0547f32292f493acedbe928
Tejas Narayanan <tejasn100@gmail.com>
2019-10-01 19:28:06 -0700

    Add mecanum drive objects

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,18 +7,38 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * An example subsystem. You can replace me with your own Subsystem.
  */
 public class DriveSubsystem extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	CANSparkMax frontLeftMotor;
+	CANSparkMax frontRightMotor;
+	CANSparkMax backLeftMotor;
+	CANSparkMax backRightMotor;
 
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
+	MecanumDrive mecanumDrive;
+
+	public DriveSubsystem() {
+		frontLeftMotor = new CANSparkMax(3, MotorType.kBrushless);
+		frontRightMotor = new CANSparkMax(4, MotorType.kBrushless);
+		backLeftMotor = new CANSparkMax(5, MotorType.kBrushless);
+		backRightMotor = new CANSparkMax(6, MotorType.kBrushless);
+
+		mecanumDrive = new MecanumDrive(frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor)
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
